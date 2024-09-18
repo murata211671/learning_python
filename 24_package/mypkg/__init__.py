27817ef3830b895003657adf3c6422c78619b86c
murata211671 <murata.tR10468@jri.co.jp>
2024-09-18 17:32:36 +0900

    commit 9/18 17:32

--- 24_package/mypkg/__init__.py
+++ 24_package/mypkg/__init__.py
@@ -10,12 +10,22 @@ import greet
 import mypkg.mymath
 import mypkg.greet
 """
-# 相対パス
+# 相対インポート
 # モジュールやパッケージの名前に対して、相対的にモジュールを指定すること
 # __name__変数により、調べられる
-print(__name__)
+print(__name__) # 変数の値が表示される　　　今回の場合はmypkg
+
+# 絶対インポートを行う場合のインポート文
+"""
 from mypkg.mymath import fact, fizzbuzz, fib
 from mypkg.greet import hello
+"""
+
+# 相対インポートを行う場合のインポート文
+
+from .mymath import fact, fizzbuzz, fib
+from .greet import hello
+
 
 
 
