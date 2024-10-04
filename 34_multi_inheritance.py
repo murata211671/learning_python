176793567ded306e1538dd4e2c2df771b943313f
murata211671 <murata.tR10468@jri.co.jp>
2024-10-04 11:54:08 +0900

    commit 2/2 --1004

--- 34_multi_inheritance.py
+++ 34_multi_inheritance.py
@@ -97,8 +97,88 @@ class D(C, B):
 d = D()
 print(D.__mro__) # class Cのインスタンスは生成される、class Bのインスタンスは生成されていない
 # print(d.b.value) # 確認-> エラー
+print()
+
+
 """
 supur()関数を用いて、すべての__init__メソッドを呼び出す必要がある
 """
 
+class B:
+    def __init__(self):
+        self.b_value = 'B'
+        print('class B init')
 
+class C:
+    def __init__(self):
+        self.c_value = 'C'
+        print('class C init')
+
+class D(C, B):
+    def __init__(self):
+        print('class D init')
+        super().__init__() ## 追加部分：super()関数で、基底クラスの__init__メソッドを呼び出している
+
+d = D()
+print(D.__mro__) # class Bの初期化が行われない
+print()
+
+class B:
+    def __init__(self):
+        self.b_value = 'B'
+        print('class B init')
+        super().__init__() ## 追加部分
+
+class C:
+    def __init__(self):
+        self.c_value = 'C'
+        print('class C init')
+        super().__init__() ## 追加部分
+
+class D(C, B):
+    def __init__(self):
+        print('class D init')
+        super().__init__()
+
+d = D()
+
+"""
+初期化の連鎖を忘れないようにすることが重要
+"""
+
+# 派生クラスから特定の基底クラスのメソッドを呼び出す
+"""
+継承構造：
+    -> B -> A -> Object
+D 
+    -> C -> Object
+"""
+
+class A:
+    def hello(self):
+        print('Hello from A')
+
+class B(A):
+    def hello(self):
+        print('Hello from B')
+        super().hello()
+
+class C:
+    def hello(self):
+        print('Hello from C')
+
+class D(B, C):
+    def hello(self):
+        print('Hello from D')
+        # super().hello() ## D,B,Aのhelloメソッドを呼び出すとき
+        # C.hello(self) ## Cの初期化を行うとき 1
+        super(A, self).hello() ## Cの初期化を行うとき 2
+
+
+d = D()
+print(D.__mro__)
+d.hello()
+
+"""
+メソッドを自分が想定したように連鎖させるには、あくまでも後からクラスを定義する側を、調整する必要がある
+"""
