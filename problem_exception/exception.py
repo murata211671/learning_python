dad7bd9bc74b91241e19adb1ec708c8caa3a4492
murata211671 <murata.tR10468@jri.co.jp>
2024-11-13 16:41:36 +0900

    commit1  --11/13

--- problem_exception/exception.py
+++ problem_exception/exception.py
@@ -33,29 +33,80 @@ print("計算を終了します。またのご利用をお待ちしておりま�
 
 ## クラスと例外クラスを使用する場合
 
-class Numlist(list) :
+# class 0：例外クラス
+class SomeException(Exception):
+    def __init__(self, *args):
+        if len(args) == 0:
+            super().__init__("数値ではありません")
+        else:
+            super().__init__(args)
+            
+
+# class 1：mixin　数字のリストなら合計を、文字のリストなら文字列を、出力する
+class Calc() :
+    def sum(self) :
+        sum = 0
+        for item in self.list1:
+            sum += item
+        print(f"sum: {sum}")
+    
+    def string(self) :
+        string = "" 
+        for item in self.list2 :
+            string += item + " "
+
+        print(f"string: {string}")
+
+# class 2：数字のリストを作成する
+class Numlist(list,Calc) :
     def __init__(self) :
-        self.list = []
+        self.list1 = []
 
     def addnum(self):
         judge = True
         while judge :
-            num = int(input("数値を入力してください-> "))
-            self.list.append(num)
-            print(f"現在のlist: {self.list}")
+            num = input("数値を入力してください-> ")
+            if not num.isnumeric() :
+                raise SomeException
+            else :
+                num = int(num)
+
+            self.list1.append(num)
+            print(f"現在のlist1: {self.list1}")
 
             if input("引き続き計算を行いますか？ -> No: 0: ") == "0" :
                 judge = False
         
         print(vars(self))
 
-class Calc(Numlist) :
-    def sum(self) :
-        sum = 0
-        for item in self.list:
-            sum += item
-        print(f"sum: {sum}")
+# class 3：文字のリストを作成する
+class Wordlist(list,Calc):
+    def __init__(self) :
+        self.list2 = []
 
-num1 = Calc()
+    def addword(self):
+        judge = True
+        while judge :
+            word = input("文字を入力してください-> ")
+            self.list2.append(word)
+            print(f"現在のlist2: {self.list2}")
+
+            if input("引き続き計算を行いますか？ -> No: 0: ") == "0" :
+                judge = False
+        
+        print(vars(self))
+    
+
+        
+
+num1 = Numlist()
 num1.addnum()
-num1.sum() 
+num1.sum()
+
+str1 = Wordlist()
+str1.addword()
+str1.string()
+
+
+
+
