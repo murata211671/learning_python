2c2db048a0ed0ab12feed3e146b674e869e5c728
murata211671 <murata.tR10468@jri.co.jp>
2024-12-18 16:52:18 +0900

    commit12/11

--- problem_class_inheritance/review28_29_class.py
+++ problem_class_inheritance/review28_29_class.py
@@ -137,3 +137,122 @@ MyClass.static_get_count()
 instance = MyClass()
 instance.static_get_count()
 
+print()
+
+class Person :
+    count = 0
+    today = datetime.datetime.today().date()
+
+    def __init__(self, dict):
+        self.fname = dict.get('fname')
+        self.sname = dict.get('sname')
+        self.age = dict.get('age')
+        self.language = dict.get('language')
+        self.birthday = dict.get('birthday')
+        self.number = Person.count
+        Person.count += 1
+    
+    def create(self, item):
+        self.list = []
+        self.list.append(item)
+        return self.list
+
+person0 = Person(dict())
+s = f"""
+Number   : {person0.number}
+-----------------------------------
+fname    : {person0.fname}, 
+sname    : {person0.sname},
+age      : {person0.age},
+language : {person0.language},
+birthday : {person0.birthday}
+-----------------------------------
+"""
+print(s)
+
+dict1 = dict(fname= 'murata', sname= 'yuuki', age= "24", language= "Japanese" ,birthday= "2000-02-01")
+
+person1 = Person(dict1)
+
+s = f"""
+Number   : {person1.number}
+-----------------------------------
+fname    : {person1.fname}, 
+sname    : {person1.sname},
+age      : {person1.age},
+language : {person1.language},
+birthday : {person1.birthday}
+-----------------------------------
+"""
+print(s)
+
+dict2 = dict(fname= 'murata', sname= 'mizuki', age= "18", language= "Japanese" ,birthday= "2006-10-19")
+
+person2 = Person(dict2)
+s = f"""
+Number   : {person2.number}
+-----------------------------------
+fname    : {person2.fname}, 
+sname    : {person2.sname},
+age      : {person2.age},
+language : {person2.language},
+birthday : {person2.birthday}
+-----------------------------------
+"""
+print(s)
+
+print(person0.create(person1))
+print(person0.create(person2))
+
+
+class Cash :
+    def __init__(self, a,b, number):
+        self.a = a
+        self.b = b
+        self.number = number
+    
+    @staticmethod
+    def create(dict):
+        cfs = []
+        number = 0
+        for value in dict.values():
+            print(value)
+            cf = Cash(value['a'],value['b'] , number)
+            cfs.append(cf)
+            number +=1
+        return cfs        
+
+a1 = dict(a=0, b=0)
+a2 = dict(a=1,b=2)
+a3 = dict(a=2,b=2)
+all = dict(cf1 = a1, cf2 = a2, cf3 = a3)
+
+cfs = Cash.create(all)
+print(cfs)
+
+cf_a_list = []
+for cf in cfs:
+    cf_a_list.append(cf.number)
+    print(f'a: {cf.a} , b: {cf.b} , number: {cf.number} ')
+    print(cf_a_list)
+
+list0 = ['USD','JPY','USD','AUD']
+list = ['USD','JPY','USD','AUD','JPY','USD','JPY','AUD','JPY','AUD']
+set1 = set()
+for item in list:
+    set1.add(item)
+print(list)
+print(set1)
+
+for item in set1 :
+    i = 0
+    count = 0
+    while i < len(list0) :
+        if list0[i] == item :
+            count +=1
+        i += 1
+    print(item)
+    print(count)
+
+
+
